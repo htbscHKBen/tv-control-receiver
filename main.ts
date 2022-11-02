7104b35a4d30a9627b7f424d4e76eed872cbdd93
htbscHKBen <116170916+htbscHKBen@users.noreply.github.com>
2022-11-02 12:16:22 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+IR.onReceivedIR(function () {
+    basic.showString(IR.getRecType())
+    basic.showString(IR.getMessage())
+})
+IR.setREC_pin(
+DigitalPin.P0
+)
+basic.forever(function () {
+	
+})
